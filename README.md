38497750a4ac9a37d05f8053e813da4ccf5eb145
Mark De May <markdemay@TirNaNog.PK5001Z>
2017-03-24 15:46:40 -0600

    Using MVC. Dividing code into model, view, and controller objects.
    Created display board.
    Created data board.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mark1ad.github.io
+
+1. Each row of the board will be in its own div to make layout easier.
--- js/app.js
+++ js/app.js
@@ -1,3 +1,79 @@
 $(function() {
   console.log('checkers connected');
+
+  controller.makeBoard();
 })
+
+// model - where the data lives
+var model = {
+  // board is 10x10. The squares on the edges are dummy squares to make
+  // life easier in other operations
+  // R - square occupied by red
+  // B - square occupied by black
+  // empty string - square unoccupied
+  constR: 'R',
+  constB: 'B',
+  constEmpty: '',
+  board: [],
+
+  makeRow: function(row, initialValue) {
+    console.log('model.makeRow');
+
+    this.board[row] = [];
+    this.board[row].length = 10;
+    this.board[row].fill(initialValue, 0, 10);
+  },
+
+  initialize: function() {
+    console.log('model.initialize()');
+
+    // make edge row and fill it with 'R'
+    this.makeRow(0, this.constR);
+
+    // make inside rows
+    for (var row = 1; row < 9; row++) {
+      this.makeRow( row, this.constEmpty);
+      // make edges occupied
+      this.board[row][0] = this.constR;
+      this.board[row][9] = this.constR;
+    }
+
+    // make last row, which is an occupied edge row
+    this.makeRow(9, this.constR);
+  }
+}
+
+// Controller - where the brains live
+var controller = {
+  makeBoard: function() {
+    console.log('controller.makeBoard()');
+
+    view.makeBoard();
+    model.initialize();
+  }
+}
+
+// view - where the display lives
+var view = {
+  // makes the board in the html
+  makeBoard: function() {
+    console.log('view.makeBoard()');
+
+    for (var rowNum = 0; rowNum < 8; rowNum++) {
+      // make a row
+      $row = $('<div>');
+      $('#board').append($row);
+      for (var colNum = 0; colNum < 8; colNum++) {
+        // make a square
+        var $square = $('<div>').addClass('board-square');
+
+        // set class for playable squares
+        if (((rowNum % 2) + colNum) % 2 === 0) {
+          $square.addClass('playable-square')
+        }
+
+        $row.append($square);
+      }
+    }
+  }
+}
